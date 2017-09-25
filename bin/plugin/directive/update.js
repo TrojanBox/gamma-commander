c5828856d319680297aa5cfe93ca2a3f07a57795
justwe9517 <justwe9517@foxmail.com>
2017-09-25 21:21:52 +0800

    增加 插件批量更新服务

---
+++ bin/plugin/directive/update.js
@@ -0,0 +1,32 @@
+let i18n = require('i18next').createInstance().init(require('../language'));
+let spawnSync = require('child_process').spawnSync;
+let path = require('path');
+
+module.exports = (pm, args) => {
+    pm.editProfileFileToJSON(data => {
+        data['plugins'] = data['plugins'] || [];
+
+        console.log();
+        for (let i in data['plugins']) if (data['plugins'].hasOwnProperty(i)) {
+            let plugin = data['plugins'][i];
+            console.log('  ' + i18n.t('installing'));
+            let s = spawnSync('npm', ['install', plugin, '--save']);
+            if (s.status === 0) {
+                try {
+                    require(path.join(plugin, 'declare.js'));
+                    console.log('  ' + plugin + ' ' + i18n.t('installSuccess'));
+                    console.log('');
+                } catch (e) {
+                    console.log('  ' + i18n.t('installFailParseError'));
+                    console.log('');
+                    spawnSync('npm', ['remove', plugin]);
+                }
+            } else {
+                console.log('  ' + i18n.t('installFail'));
+                console.log('');
+            }
+        }
+        console.log();
+        
+    }, ['plugin', 'install-plugin.json']);
+};
--- bin/plugin/index.js
+++ bin/plugin/index.js
@@ -1,4 +1,5 @@
 "use strict";
+
 let pm = require(gamma.rootdir + '/interface/profile');
 let i18n = require('i18next').createInstance().init(require('./language'));
 let Commander = require(gamma.rootdir + '/components/commander/index');
@@ -14,6 +15,7 @@ module.exports = (argv) => {
     commander.declare('list', i18n.t('pluginList'));
 
     commander.implement('install', args => require('./directive/add')(pm, args));
+    commander.implement('update', args => require('./directive/update')(pm, args));
     commander.implement('remove', args => require('./directive/remove')(pm, args));
     commander.implement('list', args => require('./directive/list')(pm, args));
 
